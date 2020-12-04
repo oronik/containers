34660d2b1044e2676331aa8b1e36b493912ac784
j-darocha <jdarocha1@gmail.com>
2020-12-04 12:55:59 +0000

    added other R deps for qualimap

--- qualimap/2.2.1/install_dep.R
+++ qualimap/2.2.1/install_dep.R
@@ -1,10 +1,39 @@
+#installing deps
+#Updated from using bioclite to biocmanager
+#Supports R3.6 now
+
+# CountsQC
+
 if (!requireNamespace("BiocManager", quietly = TRUE))
-	    install.packages("BiocManager")
+    install.packages("BiocManager")
 BiocManager::install(version = "3.12")
 
-BiocManager::install("NOISeq")
 
-install.packages("XML")
+if(!require("optparse")) { 
+    install.packages("optparse", repos = "http://cran.r-project.org") 
+}
 
-BiocManager::install("Repitools")
+if(!require("NOISeq")) {
+    BiocManager::install("NOISeq")
+    
+}
+
+# Epigenetics
+
+if(!require("XML")) { 
+    install.packages("XML", repos = "http://cran.r-project.org")
+}
+   
+if(!require("Repitools")) {
+    BiocManager::install("Repitools")
+}
+
+if(!require("Rsamtools")) {
+    BiocManager::install("Rsamtools")
+}
+
+if(!require("rtracklayer")) {
+    BiocManager::install("rtracklayer")
+}
+
 
