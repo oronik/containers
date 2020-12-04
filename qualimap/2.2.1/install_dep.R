2bb25aa88cc52ad0b35d0618adcf90f0dc089eb8
hothman <houcemoo@gmail.com>
2020-12-04 06:42:44 +0200

    Add R dependencies installing file

---
+++ qualimap/2.2.1/install_dep.R
@@ -0,0 +1,10 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+	    install.packages("BiocManager")
+BiocManager::install(version = "3.12")
+
+BiocManager::install("NOISeq")
+
+install.packages("XML")
+
+BiocManager::install("Repitools")
+
